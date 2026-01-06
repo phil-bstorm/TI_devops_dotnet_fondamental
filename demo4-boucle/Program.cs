0922e187b8cdc58fd299135045d27b0f0bdaa689
Phil BStorm <philippe.haerens@bstorm.be>
2026-01-06 16:46:33 +0100

    demo boucle

---
+++ demo4-boucle/Program.cs
@@ -0,0 +1,56 @@
+﻿
+#region boucle FOR
+
+Console.WriteLine("Boucle for 0 -> 9");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"\t le compteur: {i}");
+}
+
+Console.WriteLine("Boucle for 0 -> 9");
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine($"\t le compteur: {i}");
+}
+
+Console.WriteLine("Boucle for 10 -> 0");
+for (int i = 10; i >= 0; i -= 2)
+{
+    Console.WriteLine($"\t le compteur: {i}");
+}
+#endregion
+
+#region boucle While
+{
+    bool continuer = true;
+    while (continuer)
+    {
+        Console.WriteLine("Let's go");
+        string input = Console.ReadLine();
+        if (input == "exit")
+        {
+            continuer = false;
+        }
+    }
+}
+#endregion
+
+#region boucle do...While
+{
+    Console.WriteLine("DO WHILE");
+    bool continuer = false;
+    do
+    {
+        Console.WriteLine("Let's go");
+        string input = Console.ReadLine();
+        if (input == "exit")
+        {
+            continuer = false;
+        }
+        else
+        {
+            continuer = true;
+        }
+    } while (continuer);
+}
+#endregion
