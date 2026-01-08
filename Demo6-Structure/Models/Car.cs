a2f607ccfea7e38d4c59fe850827276f56a170ce
Phil BStorm <philippe.haerens@bstorm.be>
2026-01-08 12:00:01 +0100

    Demo struct + courte intro POO

---
+++ Demo6-Structure/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace Demo6_Structure.Models
+{
+    public struct Car
+    {
+        public string Brand;
+        public string Model;
+        public string Color;
+        public int Power;
+
+        public Car(string brand, string model, string color, int power)
+        {
+            Brand = brand;
+            Model = model;
+            Color = color;
+            Power = power;
+        }
+
+        public void FaitDuBruit()
+        {
+            Console.WriteLine("Vroom.");
+            return; // optionel
+        }
+    }
+}
---
+++ Demo6-Structure/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Demo6_Structure.Models;
+
+Console.WriteLine("Hello, World!");
+
+Car c1;
+c1.Brand = "Mazda";
+c1.Model = "MX-5";
+c1.Color = "Red";
+c1.Power = 121;
+
+Car c2 = new Car("Toyota", "Aygo", "Red", 90);
+Car c3 = new Car(color:"Red", model:"Aygo",  power:90, brand:"Toyota");
+
+Console.WriteLine($"{c1.Brand}, {c1.Model}");
+c1.FaitDuBruit();
+Console.WriteLine(Exemple());
+
+string Exemple()
+{
+    return "Je suis un exemple.";
+}
