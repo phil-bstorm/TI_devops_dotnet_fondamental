3ac9e02bc0c01a051dbcff29fbc501b695b6d656
Phil BStorm <philippe.haerens@bstorm.be>
2026-01-12 09:33:24 +0100

    Demo Enum

---
+++ Demo8-Enum/CustomEnums/PieceEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo8_Enum.CustomEnums
+{
+    public enum PieceEnum
+    {
+        // Note: la valeur attribuée aux noms n'est pas obligatoire
+        Pion = 1,
+        Tour = 5,
+        Fou = 6,
+        Cavalier = 7,
+        Reine = 11,
+        Roi = 12,
+    }
+}
---
+++ Demo8-Enum/Program.cs
@@ -0,0 +1,39 @@
+﻿using Demo8_Enum.CustomEnums;
+
+namespace Demo8_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PieceEnum p1 = PieceEnum.Reine;
+
+            Console.WriteLine("Valeur 1");
+            foreach (int item in Enum.GetValues(typeof(PieceEnum)))
+            {
+                Console.WriteLine($"- {item} / {(PieceEnum)item}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Valeur 2");
+            foreach (int item in Enum.GetValues<PieceEnum>())
+            {
+                Console.WriteLine($"- {item} / {(PieceEnum)item}");
+            }
+            Console.WriteLine();
+
+            // Parcourir les noms de l'enum
+            Console.WriteLine("Nom 1");
+            foreach (string item in Enum.GetNames(typeof(PieceEnum)))
+            {
+                Console.WriteLine($"- {item}");
+            }
+
+            Console.WriteLine("Nom 2");
+            foreach (string item in Enum.GetNames<PieceEnum>())
+            {
+                Console.WriteLine($"- {item}");
+            }
+        }
+    }
+}
